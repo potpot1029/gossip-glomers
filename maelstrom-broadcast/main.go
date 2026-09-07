7c04e5f4c4d70126629f46d2e6aeb4118e44f6b5
potpot1029 <chaukinyi1029@gmail.com>
2026-09-07 23:22:40 +0800

    impl(broadcast): code for challenge #3b: multi-node broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -13,6 +13,7 @@ type Node struct {
 	messages     []int
 	seenMessages map[int]bool
 	mu           sync.RWMutex
+	topology     map[string][]string
 }
 
 type BroadcastBody struct {
@@ -30,6 +31,7 @@ func main() {
 		node:         maelstrom.NewNode(),
 		messages:     make([]int, 0),
 		seenMessages: make(map[int]bool),
+		topology:     make(map[string][]string),
 	}
 
 	n.node.Handle("broadcast", func(msg maelstrom.Message) error {
@@ -42,6 +44,19 @@ func main() {
 		if !n.seenMessages[body.Message] {
 			n.messages = append(n.messages, body.Message)
 			n.seenMessages[body.Message] = true
+
+			for _, neighbor := range n.topology[n.node.ID()] {
+				if neighbor == msg.Src || neighbor == n.node.ID() {
+					continue
+				}
+
+				n.node.RPC(neighbor, map[string]any{
+					"type":    "broadcast",
+					"message": body.Message,
+				}, func(msg maelstrom.Message) error {
+					return nil
+				})
+			}
 		}
 		n.mu.Unlock()
 
@@ -75,6 +90,8 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
+
+		n.topology = body.Topology
 
 		resp := map[string]any{
 			"type": "topology_ok",
