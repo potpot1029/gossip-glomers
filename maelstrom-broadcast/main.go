265ce1e4b133eea86496b6f713b3bd7aec06c46d
potpot1029 <chaukinyi1029@gmail.com>
2026-09-07 23:48:18 +0800

    impl(broadcast): code for challenge #3c: fault tolerant broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -4,16 +4,18 @@ import (
 	"encoding/json"
 	"log"
 	"sync"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 type Node struct {
-	node         *maelstrom.Node
-	messages     []int
-	seenMessages map[int]bool
-	mu           sync.RWMutex
-	topology     map[string][]string
+	node            *maelstrom.Node
+	topology        map[string][]string
+	messages        []int
+	seenMessages    map[int]struct{}
+	pendingMessages map[string]map[int]struct{}
+	mu              sync.RWMutex
 }
 
 type BroadcastBody struct {
@@ -26,12 +28,40 @@ type TopologyBody struct {
 	Topology map[string][]string `json:"topology"`
 }
 
+func (n *Node) retryLoop() {
+	ticker := time.NewTicker(200 * time.Millisecond)
+	defer ticker.Stop()
+	for range ticker.C {
+		n.mu.Lock()
+
+		for neighbor, messages := range n.pendingMessages {
+			for message := range messages {
+				n.node.RPC(neighbor, map[string]any{
+					"type":    "broadcast",
+					"message": message,
+				}, func(msg maelstrom.Message) error {
+					n.mu.Lock()
+					defer n.mu.Unlock()
+
+					delete(n.pendingMessages[neighbor], message)
+
+					return nil
+				})
+			}
+		}
+
+		n.mu.Unlock()
+	}
+
+}
+
 func main() {
 	n := &Node{
-		node:         maelstrom.NewNode(),
-		messages:     make([]int, 0),
-		seenMessages: make(map[int]bool),
-		topology:     make(map[string][]string),
+		node:            maelstrom.NewNode(),
+		messages:        make([]int, 0),
+		seenMessages:    make(map[int]struct{}),
+		pendingMessages: make(map[string]map[int]struct{}),
+		topology:        make(map[string][]string),
 	}
 
 	n.node.Handle("broadcast", func(msg maelstrom.Message) error {
@@ -41,21 +71,32 @@ func main() {
 		}
 
 		n.mu.Lock()
-		if !n.seenMessages[body.Message] {
+		if _, ok := n.seenMessages[body.Message]; !ok {
 			n.messages = append(n.messages, body.Message)
-			n.seenMessages[body.Message] = true
+			n.seenMessages[body.Message] = struct{}{}
 
 			for _, neighbor := range n.topology[n.node.ID()] {
 				if neighbor == msg.Src || neighbor == n.node.ID() {
 					continue
 				}
 
+				if n.pendingMessages[neighbor] == nil {
+					n.pendingMessages[neighbor] = make(map[int]struct{})
+				}
+				n.pendingMessages[neighbor][body.Message] = struct{}{}
+
 				n.node.RPC(neighbor, map[string]any{
 					"type":    "broadcast",
 					"message": body.Message,
 				}, func(msg maelstrom.Message) error {
+					n.mu.Lock()
+					defer n.mu.Unlock()
+
+					delete(n.pendingMessages[neighbor], body.Message)
+
 					return nil
 				})
+
 			}
 		}
 		n.mu.Unlock()
@@ -99,6 +140,8 @@ func main() {
 
 		return n.node.Reply(msg, resp)
 	})
+
+	go n.retryLoop()
 
 	if err := n.node.Run(); err != nil {
 		log.Fatal(err)
